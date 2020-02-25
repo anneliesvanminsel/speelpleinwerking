48b6cdb3f072df986851d42ae32207830c56b760
Annelies <anneliesvanminsel@outlook.com>
2020-02-25 18:01:19 +0100

    Search kids by day

--- project/app/Http/Controllers/AdminController.php
+++ project/app/Http/Controllers/AdminController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Admin;
+use App\Day;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -16,5 +19,15 @@ class AdminController extends Controller
 	public function getOldAdmins(){
 		$admins = Admin::where('isActive', '=', 0)->paginate(20);
 		return view('content.admin.overview-old', ['admins' => $admins]);
+	}
+
+	public function postSearchKids(Request $request){
+		$day_id = $request->input('day');
+		$user = User::findOrFail(Auth::id());
+
+		$days = Day::orderBy('date', 'asc')->get();
+		$kids = Day::findOrFail($day_id)->kids()->get();
+
+		return view('content.admin.dashboard', ['user_id' => $user['id'], 'days' => $days, 'kids' => $kids]);
 	}
 }
